5848d02e799214eb0d305d0cfbd470ef7f9cc9b5
Guang Yang <garry.yangguang@gmail.com>
2015-11-01 20:43:33 -0800

    Test baking new AMIs with python installs

---
+++ packer_ami_fun/build_packer_image.sh
@@ -0,0 +1,4 @@
+packer build \
+  -var "aws_access_key=$AWS_ACCESS_KEY_ID" \
+  -var "aws_secret_key=$AWS_SECRET_ACCESS_KEY" \
+  python_dependencies_packer.json
---
+++ packer_ami_fun/install_python_dependencies.sh
@@ -0,0 +1,6 @@
+sudo apt-get -y update
+sudo apt-get -y install python-dev python-pip
+sudo apt-get -y install python-numpy
+sudo apt-get -y install python-scipy
+sudo apt-get -y install python-pandas
+sudo pip install "ipython[notebook]"
